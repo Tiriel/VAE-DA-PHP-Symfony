5c078cd1d2a6b7023ef3659fa338f69e1f43e294
Benjamin Zaslavsky <bzaslavsky@omnilog.fr>
2017-06-29 16:50:11 +0200

    [#P3] First draft for routing

--- project/index.php
+++ project/index.php
@@ -4,18 +4,14 @@ use Lib\CoreUtils\SplClassLoader;
 use Lib\HttpComponent\Request;
 use Lib\HttpComponent\Response;
 use Lib\HttpComponent\Router;
-use Lib\Controller;
 
 
 require_once __DIR__ . '/Lib/CoreUtils/SplClassLoader.php';
 $loader = new SplClassLoader('Lib', './Lib');
 $loader->register();
 
-$request = new Request();
-$action  = Router::route($request);
-
-$controller = new Controller();
+$request  = new Request();
 /* @var Response $response */
-$response   = $controller->$action($request);
+$response = Router::route($request);
 
 $response->send();
